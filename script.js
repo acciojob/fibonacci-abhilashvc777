730230402fe946c73f1803e530303d3ec5443bba
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-08 13:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-function fibonacci(num) {
+ function fibonacci(num){
 // your code here
-	let arr = []
-	let j = 0;
-	let k = 1
-	for(let i=1; i<num+1; i=k){
-		let sum = i+j
-		arr.push(sum)
-		j=i;
-		k=sum
-		if(num==sum){
-			break;
-		}
+	let j = 0; //i=1,j=0,k=1--i=2,sum=1,j=1,k=1---i=3,sum=2,j=1,k=2,---i=4,sum=3,j=2,k=3--i=5,sum=5,j=3,k=5--i=6,sum=8,j=5,k=8
+	let k = 1;
+	for(let i=2; i<=num; i++){
+        let sum;
+        sum=j+k
+        j = k
+        k = sum
+        // console.log(sum)
+        // console.log(k)
 	}
-	return arr.indexOf(num)
+	return j
 }
 module.exports = fibonacci;
